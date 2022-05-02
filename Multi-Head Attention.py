f79d41648df6aecd4cd6d83249901c5bc89e53af
Sahil Datta <37915188+awaknd-1@users.noreply.github.com>
2022-05-02 23:52:34 +0100

    Update Multi-Head Attention.py

--- Multi-Head Attention.py
+++ Multi-Head Attention.py
@@ -1,5 +1,17 @@
 
 
+
+def feature_extraction(inputx, neurons_1, neurons_2):
+  # extract features across electrode/ channels dimensions
+  Cout = TimeDistributed(Dense(neurons_2, activation='elu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros'))(inputx)
+  # extract features across spectral dimensions
+  F_inputs = K.permute_dimensions(Cout, (0,1,3,2))
+  # apply dense
+  Fout = TimeDistributed(Dense(neurons_1,activation='elu', kernel_initializer='he_normal', use_bias=True, bias_initializer='zeros'))(F_inputs)
+  # swap the dimensions back to the orignal settings
+  Fout = K.permute_dimensions(Fout, (0,1,3,2))
+  return Fout
+
 def MHDAN(inputx, neurons_1, neurons_2, n, h):
   # divide the vector into n heads, each for seprate attention 
   d2, d3 = inputx.shape[2], inputx.shape[3]
@@ -21,3 +33,5 @@ def MHDAN(inputx, neurons_1, neurons_2, n, h):
   # apply fully connected layers to spectral and channel/electrodes dimension
   output = feature_extraction(refined_out, neurons_1, neurons_2)
   return output
+
+
