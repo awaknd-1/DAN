f6aa574773893720184b9aa24734ea74bff84b84
Sahil Datta <37915188+awaknd-1@users.noreply.github.com>
2022-05-02 23:49:36 +0100

    Update Multi-Head Attention.py

--- Multi-Head Attention.py
+++ Multi-Head Attention.py
@@ -1,26 +1,23 @@
-# performs multi-head attention on the spectro-spatial feature map.
-def multi_head_attention(inputx, out, num_head, Atype):
+
+
+def MHDAN(inputx, neurons_1, neurons_2, n, h):
   # divide the vector into n heads, each for seprate attention 
-  d1 = tensorflow.shape(out)[0]
+  d2, d3 = inputx.shape[2], inputx.shape[3]
   out1 = list()
-  # spatial or channel
-  if Atype == "Spectral": 
-    # divide the vector into n heads, each for seprate attention 
-    new_out = tensorflow.reshape(out, shape= (d1,out.shape[1],out.shape[2]//num_head, out.shape[3], num_head))
-    output11 = attention_layer2(new_out[:,:,:,:,0], inputx[:,:,:out.shape[2]//num_head,:])
-    output12 = attention_layer2(new_out[:,:,:,:,1], inputx[:,:,out.shape[2]//num_head:,:])
-    output1 = Add()([output11, output12])
-    out1.append(output1)
-    output = Concatenate(axis=3)(out1)
-    output = tensorflow.reshape(output, shape = (d1, out.shape[1], 1, out.shape[2]))
-  elif Atype == "Channel": 
-    new_out = tensorflow.reshape(out, shape= (d1,out.shape[1],out.shape[2],out.shape[3]//num_head, num_head))
-    output11 = attention_layer2(new_out[:,:,:,:,0], inputx[:,:,:,:out.shape[3]//num_head])
-    output12 = attention_layer2(new_out[:,:,:,:,1], inputx[:,:,:,out.shape[3]//num_head:])
-    output1 = Add()([output11, output12])
-    out1.append(output1)
-    output = Concatenate(axis=3)(out1)
-    output = tensorflow.reshape(output, shape= (d1, out.shape[1], out.shape[2], out.shape[3]))
-  else:
-    raise ValueError('A very specific bad thing happened.')
+  # divide the vector into n heads, each for seprate attention 
+  # spectral attention over multiple heads
+  F1, F2, F3 = F_attention(inputx[:,:,:d2//h, :d3//h]), F_attention(inputx[:,:,d2//h:d2//h+d2//h, d3//h:d3//h+d3//h]), F_attention(inputx[:,:,d2//h+d2//h:, d3//h+d3//h:])
+  # combine the heads across spectral dimension
+  F = Concatenate(axis = 2)([F1, F2, F3])
+  # attention across channels/electrodes
+  C1, C2, C3 =  C_attention(inputx[:,:,:d2//h, :d3//h], n, ratio=8),  C_attention(inputx[:,:,d2//h:d2//h+d2//h, d3//h:d3//h+d3//h], n, ratio=8),  C_attention(inputx[:,:,d2//h+d2//h:, d3//h+d3//h:], n, ratio=8)
+  # combine the heads across channel dimension
+  C = Concatenate(axis = -1)([C1, C2, C3])
+  # # combine the two attention maps
+  Att_out = Multiply()([F, C])
+  Att_out = K.permute_dimensions(Att_out, (0,1,3,2))
+  # add the orignal feature map to attention feature map
+  refined_out = Add()([inputx, Att_out])
+  # apply fully connected layers to spectral and channel/electrodes dimension
+  output = feature_extraction(refined_out, neurons_1, neurons_2)
   return output
