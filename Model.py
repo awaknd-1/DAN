beea6fa353a978c339310d2b6b49855ee2310397
Sahil Datta <sahildatta29@gmail.com>
2022-08-27 20:09:00 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,6 +1,4 @@
-
-
-
+from Multi-Head-Attention import DAN
 
 # define the model
 def define_model(trainX):
@@ -8,53 +6,45 @@ def define_model(trainX):
     _,a,b,c = trainX.shape
     inputs1 = Input(shape=(a,b,c))
     # Block-1
-    out1 = MHDAN(inputs1,b,c, 15, 3)
+    out1 = DAN(beta=1.8).MHDAN(inputs1,b,c, 15, 3)
     out = TimeDistributed(BatchNormalization(center = True, scale = True))(out1)
     out = Add()([inputs1, out])
-    out = Activation('elu')(out)
-    #out = conv_block(out,64, 3, 2)
-    #out = residual_module(out,128)
+    out = Activation('relu')(out)
     # maxpool
-    #out = conv_block(out,128, 3, 2)
-    out = feature_extraction(out,128,128)
+    out = DAN(beta=1.8).feature_extraction(out,128,128)
     out1 = TimeDistributed(BatchNormalization(center = True, scale = True))(out)
     out = Add()([out, out1])
-    out = Activation('elu')(out)
+    out = Activation('relu')(out)
     # Block-2
-    out = feature_extraction(out,50,50)
+    out = DAN(beta=1.8).feature_extraction(out,50,50)
     out1 = TimeDistributed(BatchNormalization(center = True, scale = True))(out)
     out = Add()([out, out1])
-    out = Activation('elu')(out)
+    out = Activation('relu')(out)
     # block-3
     out = TimeDistributed(Flatten())(out)
-    out = TimeDistributed(Dense(256,activation = 'elu'))(out)
-    #embed = positional_encoding(a, 256)
-    #out = Add()([out,embed])
-    # the attention adjusted output state
-    # the softmax taking the above op
-    output = attention_layer(out)
-    output = (Dense(128,activation = 'elu'))(output)
+    out = TimeDistributed(Dense(256,activation = 'relu'))(out)
+    output = DAN(beta=1).temporal_attention(out)
+    output = Flatten()(output)
+    output = (Dense(128,activation = 'relu'))(output)
     # network-2
-    outputs = (Dense(64, activation = 'elu'))(output)
+    outputs = (Dense(64, activation = 'relu'))(output)
     outputs = (Dense(2, activation = 'sigmoid'))(outputs)
     model = Model(inputs = inputs1, outputs = outputs)
     # compile
     optim = Adam(0.001)
     model.compile(loss = 'binary_crossentropy', optimizer = optim, metrics = ['accuracy'])
     return model
-    
-# trainX and trainy are input 4D matrices of dimenions [samples, time, frequency, channel]
-def model_fit(trainX, testX, trainy, testy, e, bs):
+
+
+def model_fit(trainX, testX, trainy, testy):
     model =  define_model(trainX)
     # fit the model on the datasets
-    model.fit(trainX, trainy, epochs = e, batch_size = bs, shuffle = True, verbose = 0)
+    model.fit(trainX, trainy, epochs = 30, batch_size = 32, shuffle = True, verbose = 1)
+    #model.save('multi_freq_fusion.h5')
     # calcule the accuracy of the model
-    _, acc = model.evaluate(testX, testy, batch_size = 64, verbose = 0)
+    _, acc = model.evaluate(testX, testy, batch_size = 32, verbose = 0)
     # shows whcih class was classified correctly
     yhat_classes = model.predict(testX, verbose=0)
     yhat_classes = np.argmax(yhat_classes, axis=1)
     del model
     return acc, yhat_classes
-  
-  
-  
